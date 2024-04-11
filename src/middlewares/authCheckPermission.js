c3c60b744ff87bdd789e9adfa43f06c7bb631047
vuongdinhlinh <vuongdinhlinh1412@gmail.com>
2024-04-11 22:05:26 +0700

    update handle err in auth

--- src/middlewares/authCheckPermission.js
+++ src/middlewares/authCheckPermission.js
@@ -3,38 +3,27 @@ import jwt from "jsonwebtoken";
 
 const checkPermission = async (req, res, next) => {
   try {
-    // console.log(req.headers.authorization?.split(" "));
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-      return res.status(401).json({
-        message: "No Auth"
-      });
+      throw new Error("No Auth");
     }
     console.log(token);
     console.log(process.env.SECRETKEY);
     const data = jwt.verify(token, process.env.SECRETKEY);
     if (!data) {
-      return res.status(401).json({
-        message: "No Authorization"
-      });
+      throw new Error("No Authorization");
     }
     const user = await User.findById(data.id).populate("role");
     if (!user) {
-      return res.status(404).json({
-        message: "Not Found"
-      });
+      throw new Error("Not Found");
     }
     if (user.role.rolekey === 1) {
       next();
     } else {
-      return res.status(403).json({
-        message: "Bạn cần đăng nhập với quyền admin để làm điều này"
-      });
+      throw new Error("Bạn cần đăng nhập với quyền admin để làm điều này");
     }
   } catch (error) {
-    return res.status(400).json({
-      message: error.message
-    });
+    throw new Error(error.message);
   }
 };
 export { checkPermission };
